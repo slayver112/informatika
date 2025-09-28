991cb956d6944dcfda7897aebc194f35a04139bd
slayver112 <66054478+slayver112@users.noreply.github.com>
2025-09-28 12:12:53 +0300

    Add files via upload

---
+++ 123213123.cpp
@@ -0,0 +1,19 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <cmath>
+
+using namespace std;
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    float x, y;
+    cin >> x >> y;
+    float r = sqrt(pow(x, 2) + pow(y, 2)) / pow(2, log2(x));
+    float s = log(x) * tan(y);
+    cout << r << s;
+    float c = max(r, s);
+    cout << c;
+    cin.get();
+    return 0;
+}
