30207528da860a6e0dd7638e28ff71f18d39d1ae
slayver112 <66054478+slayver112@users.noreply.github.com>
2026-03-01 20:50:09 +0300

    Add files via upload

---
+++ alg/lab2/asdasd.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+#include <cmath>
+
+double simpson_rule(double (*f)(double), double a, double b, int n) {
+    if (n % 2 == 1) n++; // Делаем n четным
+    double h = (b - a) / n;
+    double sum = f(a) + f(b);
+
+    for (int i = 1; i < n; i += 2)
+        sum += 4 * f(a + i * h);
+
+    for (int i = 2; i < n - 1; i += 2)
+        sum += 2 * f(a + i * h);
+
+    return (h / 3) * sum;
+}
+
+double function1(double x) {
+    return 3*pow(x,2) - 2*x - 6; // Интегрируемая функция
+}
+
+double function2(double x) {
+    return -pow(x,2) + 6*x + 3; // Интегрируемая функция
+}
+
+double diff(double x) {
+    return function2(x) - function1(x);
+}
+
+int main() {
+    double a = 1.0;
+    double b = 2.0;
+    double area = simpson_rule(diff, a, b, 100);
+    std::cout << "Приближенное значение интеграла: " << area << std::endl;
+    return 0;
+}
