633afba79cca565fbd67e8c33fdcfba0fc0dadfc
slayver112 <66054478+slayver112@users.noreply.github.com>
2026-03-22 22:56:18 +0300

    Add files via upload

---
+++ alg/lab3/asdasd.cpp
@@ -0,0 +1,82 @@
+﻿#include <vector>
+#include <list>
+
+
+std::vector<int> mergeBuckets(const std::vector<std::list<int>>& buckets) {
+    std::vector<int> mergedArray;
+    for (int i = 0; i < buckets.size(); i++) {
+        std::list<int> bucket = buckets[i];
+        for (int value : bucket) {
+            mergedArray.push_back(value);
+        }
+    }
+    return mergedArray;
+}
+
+void sortByDigit(std::vector<int>& arr, int digit) {
+    if (arr.empty()) return;
+    std::vector<std::list<int>> buckets(10);
+    for (int number : arr) {
+        int bucketIndex = (number / digit) % 10;
+        buckets[bucketIndex].push_back(number);
+    }
+    arr = mergeBuckets(buckets);
+}
+
+void radixSort(std::vector<int>& arr) {
+    if (arr.empty()) return;
+    int maxNumber = arr[0];
+    int i = 1;
+    while (maxNumber / i > 0) {
+        sortByDigit(arr, i);
+        i = i * 10;
+    }
+}
+
+void combSort(std::vector<int>& arr) {
+    int n = arr.size();
+    int step = n - 1;
+
+    if (n == 2) {
+        if (arr[0] > arr[1]) {
+            std::swap(arr[0], arr[1]);
+        }
+        return;
+    }
+
+    while (step >= 1) {
+        step = step / 1.3;
+        int i = 0;
+        while (i + step < n) {
+            if (arr[i] > arr[i + step]) {
+                std::swap(arr[i], arr[i + step]);
+            }
+            i = i + 1;
+        }
+    }
+}
+
+int partition(std::vector<int>& arr, int low, int high) {
+    int pivot = arr[high];
+    int i = low - 1;
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] <= pivot) {
+            i = i + 1;
+            std::swap(arr[i], arr[j]);
+        }
+    }
+    std::swap(arr[i + 1], arr[high]);
+    return i + 1;
+}
+
+void quickSort(std::vector<int>& arr,int low,int high) {
+    if (low < high) {
+        int pivotIndex = partition(arr, low, high);
+        quickSort(arr,low,pivotIndex-1);
+        quickSort(arr,pivotIndex+1,high);
+    }
+}
+
+int main() {
+    return 0;
+}
